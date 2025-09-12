bd65300ff3d57dd4e97ef92e55ad23b79784f918
Matthew <matthewapearcy@gmail.com>
2025-09-12 05:14:42 -0400

    updated to display more charts

--- script.js
+++ script.js
@@ -1,29 +1,65 @@
 
-async function fetchData() {
-    const res = await fetch("https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=7")
+let priceChart, volumeChart;
+
+async function fetchData(coin = "bitcoin") {
+    const res = await fetch(`http://localhost:3000/api/${coin}`);
     const data = await res.json();
 
-    // Extracts the timestamps and prices
-    const labels = data.prices.map(price => new Date(price[0]).toLocaleDateString());
-    const prices = data.prices.map(price => price[1]);
+    //If API returned valid structure
+    if (!data.prices || !data.total_volumes) {
+    console.error("API error:", data);
+    alert("Failed to load data. Check backend or API.");
+    return;
+  }
 
-    renderChart(labels, prices);
+    // Extracts the timestamps and prices
+    const labels = data.prices.map(p => new Date(p[0]).toLocaleDateString());
+    const prices = data.prices.map(p => p[1]);
+    const volumes = data.total_volumes.map(v => v[1]);
+
+    renderCharts(labels, prices, volumes, coin);
 }
 
-function renderChart(labels, prices) {
-    const ctx = document.getElementById("priceChart").getContext("2d");
-    new Chart(ctx, {
+function renderCharts(labels, prices, volumes, coin) {
+    //Destroy old charts before making new ones
+    if(priceChart) priceChart.destroy();
+    if(volumeChart) priceChart.destroy();
+
+    const priceCtx = document.getElementById("priceChart").getContext("2d");
+    priceChart = new Chart(priceCtx, {
         type: "line",
         data: {
             labels: labels,
             datasets: [{
-                label: "Bitcoin Price (USD)",
+                label: `${coin.toUpperCase()} Price (USD)`,
                 data: prices,
                 borderColor: "purple",
                 fill: true
-            }]
+            }
+        ]
         }
     });
+
+    const volumeCtx = document.getElementById("volumeChart").getContext("2d");
+  volumeChart = new Chart(volumeCtx, {
+    type: "bar",
+    data: {
+      labels,
+      datasets: [
+        {
+          label: `${coin.toUpperCase()} Volume`,
+          data: volumes,
+          backgroundColor: "orange"
+        }
+      ]
+    }
+  });
 }
 
+// Dropdown event listener
+document.getElementById("coinSelect").addEventListener("change", (e) => {
+    fetchData(e.target.value);
+});
+
+// Initial load
 fetchData();
